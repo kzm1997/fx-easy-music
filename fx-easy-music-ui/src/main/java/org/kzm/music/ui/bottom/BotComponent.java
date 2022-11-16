417f098871f3458913593f5f4581e0f181056cab
kouzhengmin <1206960056@qq.com>
2022-11-16 17:41:47 +0800

    bottom

--- fx-easy-music-ui/src/main/java/org/kzm/music/ui/bottom/BotComponent.java
+++ fx-easy-music-ui/src/main/java/org/kzm/music/ui/bottom/BotComponent.java
@@ -1,6 +1,8 @@
 package org.kzm.music.ui.bottom;
 
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import org.kzm.music.ui.UIObject;
 
@@ -13,8 +15,40 @@ public class BotComponent  extends UIObject {
     
     @Override
     protected void initComponent() {
-        StackPane stackPane=new StackPane();
-        panDefaultImage = new Image("/images/topandbottom/pandefault.png");
+        HBox hBox=new HBox(92);
+        ImageView stop=new ImageView("");
+        stop.setPreserveRatio(true);
+        stop.setPickOnBounds(true);
+        stop.setFitWidth(35);
+        
+        ImageView right=new ImageView("");
+        right.setPreserveRatio(true);
+        right.setPickOnBounds(true);
+        right.setFitWidth(42);
+        
+        ImageView mode=new ImageView("");
+        mode.setPreserveRatio(true);
+        mode.setPickOnBounds(true);
+        mode.setFitWidth(21);
+        
+        ImageView volume=new ImageView("");
+        volume.setPreserveRatio(true);
+        volume.setPickOnBounds(true);
+        volume.setFitWidth(18);
+        
+        ImageView list=new ImageView("");
+        list.setPreserveRatio(true);
+        list.setPickOnBounds(true);
+        list.setFitWidth(25);
+        
+        ImageView heart=new ImageView("");
+        heart.setPreserveRatio(true);
+        heart.setPickOnBounds(true);
+        heart.setFitWidth(23);
+        
+        hBox.getChildren().addAll(stop,right,mode,volume,list,heart);
+        
+        
         
     }
 
